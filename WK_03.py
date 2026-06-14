2dc193c612690831b37704166ef992924e32b8fb
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 21:41:34 +0400

    Project 5 : Social Media Profile Combine

---
+++ WK_03.py
@@ -0,0 +1,43 @@
+# Python Data Types
+# Project 1 : User profile Generator [STRING]
+
+# name = "Alice"
+
+# email = "alice@yahoo.com"
+
+# Country = "Italy"
+
+# print(name)
+# print(email)
+# print(Country)
+
+# Project 2 : Stadium Capacity Trcker [INTEGER]
+
+# total_seats = 50000
+
+# occupied_seats =  43890
+
+# print(total_seats)
+# print(occupied_seats)
+
+# Project 3 : Stock Price Monitor [FLOAT]
+
+# share_price = 124.75
+
+# print(share_price)
+
+
+# Project 4 : Login Verification [BOOLEAN]
+# is_logged_in = True
+# print(is_logged_in)
+
+# Project 5 : Social Media Profile Combine
+username = "techexpert"
+followers = 50000
+rating = 4.8
+verified = True
+
+print(username)
+print(followers)
+print(rating)
+print(verified)
