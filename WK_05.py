893857c5499e481afb2ddfbf2d52a81930f32ee7
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 22:21:16 +0400

    AI Token Cost Calculator

---
+++ WK_05.py
@@ -0,0 +1,50 @@
+# Working with Operators [ARITHMETIC]
+# Project 1: Employee Salary Calculator
+# hours = 160
+# rate  = 20
+# salary = hours * rate
+# print(salary)
+
+# Addition
+# Bank Account
+# balance = 5000
+# deposit = 2000
+# new_balance = balance + deposit
+# print(new_balance)
+
+# Subtraction 
+# Inventory System
+
+# stock = 500
+# sold = 120
+# remaining_stock = stock - sold
+# print(remaining_stock)
+
+# Multiplication
+# Cinema Revenue
+# ticket_price = 10
+# ticket_sold = 800
+
+# revenue = ticket_price * ticket_sold
+# print(revenue)
+
+# Division
+# Average Score
+# total_marks = 450
+# subjects = 5
+# average = total_marks / subjects
+# print(average)
+
+# Modulus
+# Even odd Checker
+# number = 17
+# remaining = number % 2
+# print(remaining)
+
+# Combine
+# AI Token Cost Calculator
+tokens = int(input("Tokens Used: "))
+price = 0.002
+cost = (tokens / 1000) *price
+
+print(f"Total Cost: {cost}")
