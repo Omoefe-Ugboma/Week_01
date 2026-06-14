a780e20c86c582aab4900b47e7fb44a8332b6ae5
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 23:37:22 +0400

    AI Chat Session

---
+++ WK_Day_08.py
@@ -0,0 +1,39 @@
+# Working Loops
+# Generate Employee IDs
+# for number in range(1, 11):
+#     print("EMP", number)
+
+# AI Training Epoch Simulation
+# for epoch in range(1,6):
+#     print("Training Epoch", epoch)
+
+# Multiplication Table Generator
+# number = int(input("Enter Number: ")) 
+# for i in range(1, 13):
+#     print(number,"X", i, "=", number * i) 
+
+# Revenue Report Generator
+# sales = [5000, 8000, 6000, 10000, 12000]
+
+# for amount in sales:
+#     print(amount)
+
+# Working WHILE LOOP
+# Password REtry System
+# password = ""
+# while password != "admin123":
+#     password = input("Enter Password: ")
+# print("Access Granted")
+
+# ATM PIN Validation
+# pin = ""
+# while pin != "1234":
+#     pin = input("Enter PIN: ")
+# print("Login Successful")
+
+# AI Chat Session
+message = ""
+while message != "exit":
+    message = input("You: ")
+    print("AI: ", message )
+print("Session Ended")
