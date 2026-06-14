efdc00ff15749342f6d2b8fa2d845cb080790cbd
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 20:50:12 +0400

    Project 1: Digital Receipt

---
+++ WK_01.py
@@ -0,0 +1,22 @@
+# Working with the print()
+# print("Hello World")
+# print(23+34)
+# print("=" * 10)
+# print("Welcome.....")
+# print("The server is loading, Please waiting for a moment!!!")
+
+
+# Project 1: Digital Receipt
+print("=" * 50)
+print("          AMAZON DIGITAL RECEIPT")
+print("=" * 50)
+
+print("Customer  :  John Michelle")
+print("Product   :  Laptop")
+print("Price     :  $1200")
+
+print("-" * 50)
+
+print("Thank you for shopping with us")
+
+print("=" * 50)
