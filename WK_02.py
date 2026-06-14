40133152090a1859a31955078dc6290f1d43d07a
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 21:23:23 +0400

    Project 3 : AI API Cost Estimator

---
+++ WK_02.py
@@ -0,0 +1,24 @@
+# Working with Variable
+# First_Name = "Omoefe"
+# Age = 29
+# Country = "United Kingdom"
+# Qaulification = "Bachelor of Sscience"
+
+# print(f"I am {First_Name}, and I am {Age} years old, from the {Country}, and my highest Q {Qaulification}")
+
+# Project 2: Employee Payroll Calculator
+# employee_name = "Emmanuel"
+# hours_worked = 180
+# hourly_rate = 25
+# salary = hours_worked * hourly_rate
+
+# print(employee_name)
+# print(salary) 
+
+# Project 3 : AI API Cost Estimator
+tokens_used = 500000
+cost_per_1000 = 0.002
+
+total_cost = (tokens_used / 1000) * cost_per_1000
+
+print(total_cost)
