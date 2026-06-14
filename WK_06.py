b62bb5e8ab49a2afa105af65fce7b14e54b166d3
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 22:34:53 +0400

    University Admission Checker

---
+++ WK_06.py
@@ -0,0 +1,20 @@
+# Working With Comparison Operator
+
+# password = "admin123"
+
+# entered_password = input("Password: ")
+
+# print(entered_password == password)
+
+# Voting System
+# age = int(input("Enter your age: "))
+
+# print(age >= 18)
+
+# Loan Approval Checker
+# salary = int(input("Enter your salary: "))
+# print(salary > 30000)
+
+# University Admission Checker
+score = int(input("Exam Score: "))
+print(score >=75)
