f285ceddcda020eb98196e1f1199e2c88e985b9d
Omoefe-Ugboma <jkomoefey23e@yahoo.co.uk>
2026-06-14 22:02:19 +0400

    Project : 3 Restaurant Order System

---
+++ WK_04.py
@@ -0,0 +1,29 @@
+# Working INPUT()
+# Project : 1 Personal AI Assistant
+
+# name = input("What is your name?")
+
+# print(f"Welcome, {name}")
+
+# Project : 2 Employee Registration System
+# employee_name = input("Enter employee name: ")
+# department = input("Enter department: ")
+
+# print()
+# print("Employee Registered")
+# print(employee_name)
+# print(department)
+
+# Project : 3 Restaurant Order System
+# customer_name = input("Customer Name: ")
+# food = input("Food Odered: ")
+# qaunity = input("Quatity: ")
+
+# print()
+# print("ORDER  SUMMARY")
+# print(customer_name)
+# print(food)
+# print(qaunity)
+
+age = int(input("Enter your age"))
+print(age + 5)
